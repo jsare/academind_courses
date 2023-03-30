273cb3b7368f3065b648e5f98afe35e1a852e15f
Sergey Klimanoff <iamjsare@gmail.com>
2023-03-30 12:04:18 -0600

    Creating a Shop App

---
+++ shop_app/lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'screens/home_screen.dart';
+
+class ShopApp extends StatelessWidget {
+  const ShopApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shop App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
