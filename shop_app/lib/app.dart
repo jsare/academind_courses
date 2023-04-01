bf8826ce2379ccf79000d8abbcba95f6e11d28be
Sergey Klimanoff <iamjsare@gmail.com>
2023-03-31 20:23:19 -0600

    fix some changes

--- shop_app/lib/app.dart
+++ shop_app/lib/app.dart
@@ -2,23 +2,29 @@ import 'package:flutter/material.dart';
 
 import 'screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
+
+import './providers/products.dart';
 
 class ShopApp extends StatelessWidget {
   const ShopApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shop App',
-      theme: ThemeData(
-        useMaterial3: true,
-        colorSchemeSeed: Colors.purple,
-        fontFamily: 'Lato',
+    return ChangeNotifierProvider(
+      create: (context) => Products(),
+      child: MaterialApp(
+        title: 'Shop App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorSchemeSeed: Colors.purple,
+          fontFamily: 'Lato',
+        ),
+        home: const ProductsOverwiewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverwiewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => const ProductDetailScreen(),
-      },
     );
   }
 }
